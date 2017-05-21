3626a5c74cb579bc786da640b038f77a2495b73a
David Bananier <David.Bananier@gmail.com>
2017-05-21 11:27:19 +0200

    Added Javadoc comments and Javadoc for ILabyrinthe Labyrinthe
    Writed Javadoc comments and created Javadoc for the Labyrinthe Class and
    the ILabyrinthe Interface

--- LabyrintheP/src/labyrinthe_manager/ILabyrinthe.java
+++ LabyrintheP/src/labyrinthe_manager/ILabyrinthe.java
@@ -1,11 +1,38 @@
 package labyrinthe_manager;
 
+import case_manager.Case;
+
+/**
+ * <h1>Interface ILabyrinthe</h1>
+ * <p>Interface d&eacutecrivant les principales op&eacuterations possible pour un labyrinthe. Il y en a trois :
+ * <ol>
+ * 	<li>Lecture du fichier pour contruire le labyrinthe</li>
+ * 	<li>Affichage du Labyrinthe</li>
+ * 	<li>Parcours en profondeur du Labyrinthe pour trouver la sortie</li>
+ * </ol>
+ * </p>
+ * @author Bananier David
+ * @see case_manager.ICase
+ */
 public interface ILabyrinthe {
-
+	/**
+	 * <h1>M&eacutethode lireFichier()</h1>
+	 * <p>Construit le labyrinthe a partir d'un fichier formatt&eacute</p>
+	 * @param nomFichier nom du fichier dans lequel est stock&eacute le Labyrinthe.
+	 */
 	public abstract void lireFichier(String nomFichier);
-
+	/**
+	 * <h1>M&eacutethode afficherLabyrinthe()</h1>
+	 * <p>Affiche le Labyrinthe selon le m&ecircme format que dans le {@link ILabyrinthe#lireFichier(String) fichier} avec la possibilit&ecaute
+	 * d'afficher le chemin de sortie</p>
+	 * @param afficherSolution permet d'afficher ou non le chemin de sortie qui a &eacutet&eacute trouv&eacute lors du parcours en profondeur
+	 * @see ILabyrinthe#parcoursLabyrinthe()
+	 */
 	public abstract void afficherLabyrinthe(boolean afficherSolution);
-
+	/**
+	 * <h1>M&eacutethode parcoursLabyrinthe()</h1>
+	 * <p>Parcours le Labyrinthe en profondeur pour trouver la sortie.</p>
+	 */
 	public abstract void parcoursLabyrinthe();
 
 }
--- LabyrintheP/src/labyrinthe_manager/Labyrinthe.java
+++ LabyrintheP/src/labyrinthe_manager/Labyrinthe.java
@@ -2,23 +2,70 @@ package labyrinthe_manager;
 
 import case_manager.ICase;
 import case_manager.Case;
-
+/**
+ * <h1>Classe Labyrinthe</h1>
+ * <p>Classe repr&eacutesentant un labyrinthe sous la forme d'une matrice de {@link case_manager.ICase cases}</p>
+ * <p>Un labyrinthe peut &ecirctre d&eacutecrit par un tableau &agrave double entr&eacutee (une matrice) ou chaque case contient une
+ * instance de la classe {@link case_manager.ICase ICase}. On peut construire un labyrinthe vide dans un premier temps puis le remplir
+ * en r&eacutecup&eacuterant les donn&eacutees {@link Labyrinthe#lireFichier(String) dans un fichier texte} ou directement construire
+ * le labyrinthe &agrave l'aide du fichier</p>
+ * @author Bananier David
+ * @author Ba Mohamed
+ */
 public class Labyrinthe implements ILabyrinthe {
 
-	private ICase[][] labyrinthe;
-
+	private ICase[][] labyrinthe = new ICase[10][10];
+	private int sizeX = 0;
+	private int sizeY = 0;
+	/**
+	 * <h1>Constructeur de la classe Labyrinthe </h1>
+	 * <p>Construit le Labyrinthe &agrave partir du fichier nomFichier.</p>
+	 * <p>Utilise la m&eacutethode {@link Labyrinthe#lireFichier(String) lireFichier} pour construire le labyrinthe &agrave partir
+	 * d'un fichier.</p>
+	 * @param nomFichier chemin vers le fichier qui contient les donn&eacutees.
+	 * @see Labyrinthe#lireFichier(String)
+	 */
 	public Labyrinthe(String nomFichier) {
-
+		lireFichier(nomFichier);
 	}
-
+	/**
+	 * <h1>Constructeur de la classe Labyrinthe </h1>
+	 * <p>Construit un Labyrinthe vide. pour pouvoir cr&eacuteer le labyrinthe, il va falloir utiliser la m&eacutethode 
+	 * {@link Labyrinthe#lireFichier(String) lireFichier}.</p>
+	 * @see Labyrinthe#lireFichier(String) 
+	 */
 	public Labyrinthe() {
 
 	}
 
 
 	/**
-	 * @see labyrinthe_manager.ILabyrinthe#lireFichier(labyrinthe_manager.String)
+	 * <h1>M&eacutethode lireFichier()</h1>
+	 * <p>Construit le labyrinthe a partir d'un fichier formatt&eacute</p>
+	 * <p>Chaque Labyrinthe est stock&eacute dans un fichier texte formatt&eacute qui contient sa taille et les informations
+	 * sur chacune de ses cases. On peut par exemple avoir le contenu de fichier suivant :
+	 * <code>
+	 * <br>
+	 * 2<br>
+	 * 3<br>
+	 * +-+-+-+<br>
+	 * |. . .|<br>
+	 * + +-+ +<br>
+	 * |A|. T|<br>
+	 * + +-+-+<br>
+	 * </code>
+	 * Les deux permi&egraveres lignes vont correspondres respectivement au nombre de ligne et de colonnes du labyrinthe. Les
+	 * lignes suivantes d&eacutecrivent chaque case du labyrinthe ainsi que la case de d&eacutepart (symbolis&eacutee par un T pour
+	 * Th&eacutes&eacutee) et celle d'arriv&eacutee (symbolis&eacutee par un A pour Arianne).
+	 * </p>
+	 * @param nomFichier nom du fichier dans lequel est stock&eacute le Labyrinthe.
+	 * @see labyrinthe_manager.ILabyrinthe#lireFichier(String)
+	 * @see case_manager.ICase#setMurN(boolean)
+	 * @see case_manager.ICase#setMurS(boolean)
+	 * @see case_manager.ICase#setMurE(boolean)
+	 * @see case_manager.ICase#setMurO(boolean)
 	 */
+	@Override
 	public void lireFichier(String nomFichier) {
 
 	}
@@ -27,14 +74,43 @@ public class Labyrinthe implements ILabyrinthe {
 	/**
 	 * @see labyrinthe_manager.ILabyrinthe#afficherLabyrinthe(boolean)
 	 */
+	@Override
 	public void afficherLabyrinthe(boolean afficherSolution) {
 
 	}
 
 
 	/**
+	 * <h1>M&eacutethode parcoursLabyrinthe()</h1>
+	 * <p>Parcours le Labyrinthe en profondeur pour trouver la sortie.</p>
+	 * <p>Cette methode va parcourir le graphe representant le labyrinthe selon un parcours en profondeur, &agrave l'aide d'une 
+	 * {@link pile_manager.Pile pile}, pour trouver le chemin entre l'entr&eacutee et la sortie du labyrinthe. &Agrave chaque 
+	 * &eacutetape du parcours, cette m&eacutethode proc&egravede de la mani&egravere suivante :
+	 * <ul>
+	 * 		<li>On {@link pile_manager.IPile#empiler(Object) empile} la {@link case_manager.Case case} C &agrave laquelle on est.</li>
+	 * 		<li>Ensuite on parcours les voisins directs, c'est &agrave dire :
+	 * 			<ol>
+	 * 				<li>On regarde si C a une  case voisine CN accessible au {@link case_manager.ICase#hasMurN() nord}.</li>
+	 * 				<li>Si oui, on {@link case_manager.ICase#setPrecedent(case_manager.ICase) d&eacutefinit la pr&eacutec&eacutedente} de CN dans le parcours en profondeur par C.</li>
+	 * 				<li>Si CN n'a pas d&eacutej&agrave &eacutet&eacute visit&eacute, On parcoure CN. </li>
+	 * 				<li>Si CN {@link case_manager.ICase#isCheminSortie() est sur le chemin de sortie}, alors C aussi, on peut donc depiler et revenir en arriere.</li>
+	 * 				<li>Si le traitement continue, on fait la m&ecircme chose au sud &agrave l'est et &agrave l'ouest.</li>
+	 * 			</ol>
+	 * 		</li>
+	 * 		<li>Si le traitement est arrive jusqu'&agrave la fin, on depile et on arr&ecircte le traitement</li>
+	 * </ul>
+	 * &Agrave la fin du tra&icirctement complet, on a le chemin vers la sortie s'il y en a un.
+	 * </p>
 	 * @see labyrinthe_manager.ILabyrinthe#parcoursLabyrinthe()
+	 * @see ICase#getPrecedent()
+	 * @see ICase#hasMurN()
+	 * @see ICase#hasMurS()
+	 * @see ICase#hasMurE()
+	 * @see ICase#hasMurO()
+	 * @see ICase#isCheminSortie()
+	 * @see ICase#setCheminSortie(boolean)
 	 */
+	@Override
 	public void parcoursLabyrinthe() {
 
 	}
